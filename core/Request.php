208d2e34d0baf959f51dd54f0aeb347336cf8350
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-02-07 08:38:12 +0100

    update public/index

--- core/Request.php
+++ core/Request.php
@@ -3,8 +3,7 @@
 namespace app\core;
 class Request
 {
-    public function getPath()    
-    {
+    public function getPath()    {
         $path = $_SERVER['REQUEST_URI'] ?? '/';
         $position = strpos($path, '?');
         if ($position === false) {
@@ -13,8 +12,7 @@ class Request
         return substr($path, 0, $position);
     }
 
-    public function getMethod()
-    {
+    public function getMethod(){
         return strtolower($_SERVER['REQUEST_METHOD']);
     }
 
